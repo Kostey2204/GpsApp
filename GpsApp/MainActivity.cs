fd993c1053bf4e06e5a95c1f82ba77a2a4901f99
konstantin <kostey2204@yandex.ru>
2023-02-24 23:37:33 +0300

    add gps fuctional

--- GpsApp/MainActivity.cs
+++ GpsApp/MainActivity.cs
@@ -1,26 +1,97 @@
-﻿using Android.App;
+﻿using System;
+using System.Linq;
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using Android.Locations;
 
 namespace GpsApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, ILocationListener
     {
+        private EditText txtLatitude;
+        private EditText txtLongitude;
+        
+        Location currentLocation;  
+        LocationManager locationManager;  
+        string locationProvider;
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            
+            txtLatitude = FindViewById<EditText>(Resource.Id.latitude);
+            txtLongitude = FindViewById<EditText>(Resource.Id.longitude);
+            
+            InitializeLocationManager();
+        }
+        private void InitializeLocationManager()
+        {
+            locationManager = (LocationManager)GetSystemService(LocationService);
+
+            var criteriaForLocationService = new Criteria {Accuracy = Accuracy.Fine};
+            
+            try
+            {
+                locationProvider = locationManager.GetBestProvider(criteriaForLocationService, true);
+                if (locationManager.IsProviderEnabled(locationProvider))
+                {
+                    locationManager.RequestLocationUpdates(locationProvider, 2000, 1, this);
+                }
+                else
+                {
+                    throw new Exception("error");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
         }
         
-        public void SendMessage(View view)
+        public void OnLocationChanged(Location location)  
+        {  
+            currentLocation = location;  
+            if (currentLocation == null)  
+            {  
+                txtLatitude.Text = "error";
+                txtLongitude.Text = "error";  
+            }
+            else   
+            {  
+                txtLatitude.Text = currentLocation.Latitude.ToString();  
+                txtLongitude.Text = currentLocation.Longitude.ToString();  
+            }  
+        }
+
+        public void OnProviderDisabled(string? provider)
         {
-            EditText editText = FindViewById<EditText>(Resource.Id.latitude);
-            editText!.Text = "это тест";
+            
+        }
+
+        public void OnProviderEnabled(string? provider)
+        {
+            
+        }
+
+        public void OnStatusChanged(string? provider, Availability status, Bundle? extras)
+        {
+            
+        }
+
+        protected override void OnResume()  
+        {  
+            base.OnResume();  
+            //locationManager.RequestLocationUpdates(locationProvider, 0, 0, this);  
+        }  
+        protected override void OnPause()  
+        {  
+            base.OnPause();  
+            locationManager.RemoveUpdates(this);  
         }
     }
 }
